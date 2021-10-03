dba7a5598cdea2f46309858add0f1aa23313cadb
Raphael <rapha_rmm@hotmail.com>
2021-10-02 21:02:26 -0300

    ➕ Define global styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,16 @@
-const theme = {};
+const theme = {
+  background: '#221A37',
+  backgroundLight: '#1B2138',
+  primary: '#34B1F6',
+  secondary: '#B396FD',
+  gradientDark: 'linear-gradient(225deg, #221A37 100%, #662DFC 100%);',
+  gradientLight: 'linear-gradient(225deg, #C438EF 100%, #40DDFF 100%);',
+  text: '#B396FD',
+  textHighlight: '#444B5B',
+  textLight: '#C4C4C4',
+  border: '#313958',
+  inputBackground: '#1E253E',
+  error: '#d45d5d'
+};
 
 export default theme;
